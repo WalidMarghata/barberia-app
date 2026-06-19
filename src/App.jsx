18db80d083f4f5099ee0bfdea92b83c675bb07e5
Walid Marghata <walid.marghata@mip.com.br>
2026-06-19 13:22:09 -0300

    fix: botao lateral em italiano - Scarica App

--- src/App.jsx
+++ src/App.jsx
@@ -1384,7 +1384,7 @@ export default function App() {
               <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" y1="15" x2="12" y2="3"/>
             </svg>
           </div>
-          <span className="pwa-side-tab-text">Baixar App</span>
+          <span className="pwa-side-tab-text">Scarica App</span>
         </button>
       )}
 
